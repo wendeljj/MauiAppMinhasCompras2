f966c1b5eea247dceb74f255afdd60d195cb3d2b
wendeljj <wendelsantos905@gmail.com>
2025-03-10 02:05:12 -0300

    construção da interface gráfica necessária para preencher os dados e salvar no SQLite

--- MauiAppMinhasCompras2/App.xaml.cs
+++ MauiAppMinhasCompras2/App.xaml.cs
@@ -1,9 +1,34 @@
-﻿using MauiAppMinhasCompras2.Views;
+﻿using MauiAppMinhasCompras2.Helpers;
+using MauiAppMinhasCompras2.Views;
 
 namespace MauiAppMinhasCompras2
 {
     public partial class App : Application
     {
+        //campo estático (pertence a classe)
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get 
+            {
+                //se a instância _db não foi criada, cria uma nova instância
+                if (_db == null)
+                {
+                    //obtém o caminho do banco de dados atráves da pasta LocalApplicationData
+
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    //Cria uma nova instância do SQLiteDatabaseHelper, passando o caminho do bd
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
