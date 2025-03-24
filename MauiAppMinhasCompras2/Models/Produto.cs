a82c47311342836859990b9c77634919af932e1e
wendeljj <wendelsantos905@gmail.com>
2025-03-24 00:33:10 -0300

    classe produto atualizada

--- MauiAppMinhasCompras2/Models/Produto.cs
+++ MauiAppMinhasCompras2/Models/Produto.cs
@@ -5,6 +5,8 @@ namespace MauiAppMinhasCompras2.Models
     public class Produto
     {
         string _descricao;
+        double _quantidade;
+        double _preco;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
@@ -20,8 +22,26 @@ namespace MauiAppMinhasCompras2.Models
                 _descricao = value;
             }
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public double Quantidade { get => _quantidade;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Por favor, preencher a quantidade");
+                }
+                _quantidade = value;
+            }
+        }
+        public double Preco { get => _preco;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Por favor, preencher o preço");
+                }
+                _preco = value;
+            } 
+        }
         public double Total { get => Preco * Quantidade; }
     }
 }
